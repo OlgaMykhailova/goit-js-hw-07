46962992c4c36c68cf9e6ce8f7d97d1caa6d58b4
OlgaMykhailova <rosebud_z@yahoo.com>
2022-07-27 17:27:39 +0200

    added task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,40 +3,47 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-const galleryImages = galleryItems.map((galleryImage) => {
-  galleryImage = `<div class="gallery__item">
-    <a class="gallery__link" href="${galleryImage.original}">
-      <img
-        class="gallery__image"
-        src="${galleryImage.preview}"
-        data-source="${galleryImage.original}"
-        alt="${galleryImage.description}"
-      />
-    </a>
-  </div>`;
-  return galleryImage;
-});
-
 const divContainer = document.querySelector(".gallery");
 
-divContainer.insertAdjacentHTML("afterbegin", galleryImages.join(""));
+const galleryImages = galleryItems
+  .map(
+    (galleryImage) => `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryImage.original}">
+  <img
+    class="gallery__image"
+    src="${galleryImage.preview}"
+    data-source="${galleryImage.original}"
+    alt="${galleryImage.description}"
+   />
+</a>
+      </div>
+   
+  </div>`
+  )
+  .join("");
 
-divContainer.addEventListener("click", onClickImage);
+divContainer.innerHTML = galleryImages;
 
-function onClickImage(event) {
+divContainer.addEventListener("click", onOpenModal);
+
+function onOpenModal(event) {
   if (event.target.nodeName !== "IMG") {
-    // console.log(event.target.nodeName)
-    console.log('no')
     return;
   }
-  console.log(event.target.parentNode)
-  console.log('yes')
+
   event.preventDefault();
-  console.log(event.target.getAttribute('src'))
-  event.target.setAttribute("src", event.target.parentNode.getAttribute('href'))
-  console.log(event.target.getAttribute('src'))
-  
-//   console.log(event.target.parentNode.attributes);
-//   console.log(event.target.style.src)
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1280">
+`);
+  instance.show();
+
+  window.addEventListener("keydown", onCloseModal);
+
+  function onCloseModal(event) {
+    if (basicLightbox.visible() && event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onCloseModal);
+    }
+  }
 }
-divContainer.addEventListener("click", onClickImage)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const divContainer = document.querySelector(".gallery");
+
+const galleryImages = galleryItems
+  .map(
+    (galleryImage) => `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryImage.original}">
+  <img
+    class="gallery__image"
+    src="${galleryImage.preview}"
+    data-source="${galleryImage.original}"
+    alt="${galleryImage.description}"
+   />
+</a>
+      </div>
+   
+  </div>`
+  )
+  .join("");
+
+divContainer.innerHTML = galleryImages;
